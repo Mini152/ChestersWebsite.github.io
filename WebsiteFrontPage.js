2a45c5d5f90b613bef434a9911ab1873a132817c
Mini152 <60660223+Mini152@users.noreply.github.com>
2020-02-25 17:58:41 +0000

    Home Page
    new home page where other projects can be linked from

---
+++ WebsiteFrontPage.js
@@ -0,0 +1,89 @@
+const BtnLink1 = document.getElementById('btnLink1');
+const BtnLink2 = document.getElementById('btnLink2');
+const BtnLink3 = document.getElementById('btnLink3');
+const BtnLink4 = document.getElementById('btnLink4');
+const BtnLink5 = document.getElementById('btnLink5');
+const BtnLink6 = document.getElementById('btnLink6');
+const BtnLink7 = document.getElementById('btnLink7');
+const BtnLink8 = document.getElementById('btnLink8');
+
+//BtnLink1
+BtnLink1.addEventListener('mouseover', function() {
+    BtnLink1.style.backgroundColor = 'slategrey';
+});
+BtnLink1.addEventListener('mouseleave', function() {
+    BtnLink1.style.backgroundColor = 'lightslategrey';
+})
+BtnLink1.addEventListener('click', function() {
+    window.location.href = "Draw.html"
+})
+//BtnLink2
+BtnLink2.addEventListener('mouseover', function() {
+    BtnLink2.style.backgroundColor = 'slategrey';
+});
+BtnLink2.addEventListener('mouseleave', function() {
+    BtnLink2.style.backgroundColor = 'lightslategrey';
+})
+BtnLink2.addEventListener('click', function() {
+    
+})
+//BtnLink3
+BtnLink3.addEventListener('mouseover', function() {
+    BtnLink3.style.backgroundColor = 'slategrey';
+});
+BtnLink3.addEventListener('mouseleave', function() {
+    BtnLink3.style.backgroundColor = 'lightslategrey';
+})
+BtnLink3.addEventListener('click', function() {
+    
+})
+//BtnLink4
+BtnLink4.addEventListener('mouseover', function() {
+    BtnLink4.style.backgroundColor = 'slategrey';
+});
+BtnLink4.addEventListener('mouseleave', function() {
+    BtnLink4.style.backgroundColor = 'lightslategrey';
+})
+BtnLink4.addEventListener('click', function() {
+    
+})
+//BtnLink5
+BtnLink5.addEventListener('mouseover', function() {
+    BtnLink5.style.backgroundColor = 'slategrey';
+});
+BtnLink5.addEventListener('mouseleave', function() {
+    BtnLink5.style.backgroundColor = 'lightslategrey';
+})
+BtnLink5.addEventListener('click', function() {
+    
+})
+//BtnLink6
+BtnLink6.addEventListener('mouseover', function() {
+    BtnLink6.style.backgroundColor = 'slategrey';
+});
+BtnLink6.addEventListener('mouseleave', function() {
+    BtnLink6.style.backgroundColor = 'lightslategrey';
+})
+BtnLink6.addEventListener('click', function() {
+    
+})
+//BtnLink7
+BtnLink7.addEventListener('mouseover', function() {
+    BtnLink7.style.backgroundColor = 'slategrey';
+});
+BtnLink7.addEventListener('mouseleave', function() {
+    BtnLink7.style.backgroundColor = 'lightslategrey';
+})
+BtnLink7.addEventListener('click', function() {
+    
+})
+//BtnLink8
+BtnLink8.addEventListener('mouseover', function() {
+    BtnLink8.style.backgroundColor = 'slategrey';
+});
+BtnLink8.addEventListener('mouseleave', function() {
+    BtnLink8.style.backgroundColor = 'lightslategrey';
+})
+BtnLink8.addEventListener('click', function() {
+    
+})
