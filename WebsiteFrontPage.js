e1790fc4f3969f317ddc8866a377d6d28f14f618
Mini152 <60660223+Mini152@users.noreply.github.com>
2020-02-27 18:12:00 +0000

    More pictures on home page

--- WebsiteFrontPage.js
+++ WebsiteFrontPage.js
@@ -1,3 +1,4 @@
+//Buttons
 const BtnLink1 = document.getElementById('btnLink1');
 const BtnLink2 = document.getElementById('btnLink2');
 const BtnLink3 = document.getElementById('btnLink3');
@@ -7,6 +8,48 @@ const BtnLink6 = document.getElementById('btnLink6');
 const BtnLink7 = document.getElementById('btnLink7');
 const BtnLink8 = document.getElementById('btnLink8');
 
+//Picture
+const btnNext = document.getElementById('btnNext');
+const btnPrev = document.getElementById('btnPrev');
+const PicImg = document.getElementById('PicImg');
+var i = 0;
+const PicArray = ["https://static01.nyt.com/images/2018/05/03/us/03spongebob_xp/03spongebob_xp-articleLarge.jpg?quality=75&auto=webp&disable=upscale","https://cdn.vox-cdn.com/thumbor/ywwImRtZDaNCI6Hnwl6Bo5Z7j6I=/0x0:1920x1080/1200x800/filters:focal(807x387:1113x693)/cdn.vox-cdn.com/uploads/chorus_image/image/63718716/spongebob_characters_wallpaper_thumb_1920_637740.0.jpg","https://i.pinimg.com/originals/56/f4/dd/56f4dda3fac4ea5ef162d4d9ca3e6a6d.jpg","https://cdn.vox-cdn.com/thumbor/wnfK7HybprDP8_VZtwpiNYRByKE=/0x0:638x404/1200x800/filters:focal(268x151:370x253)/cdn.vox-cdn.com/uploads/chorus_image/image/62424386/Spongebob_Squarepants.0.png","https://media1.fdncms.com/stranger/imager/u/large/41856421/1572556602-1510603395669960874.jpg"];
+
+//Picture Shuffle
+function NextPic() {
+    if (i < PicArray.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+    PicImg.src = PicArray[i];
+}
+function PrevPic() {
+    if (i > 0) {
+        i--;
+    } else {
+        i = PicArray.length - 1;
+    }
+    PicImg.src = PicArray[i];  
+}
+function Shuffle() {
+    
+}
+function Wait(ms) {
+    var d = new Date();
+    var d2 = null;
+    do { d2 = new Date(); }
+    while(d2-d < ms);
+}
+
+btnNext.addEventListener('click', function() {
+    NextPic();
+})
+btnPrev.addEventListener('click', function() {
+    PrevPic();
+})
+
+//Buttons
 //BtnLink1
 BtnLink1.addEventListener('mouseover', function() {
     BtnLink1.style.backgroundColor = 'slategrey';
