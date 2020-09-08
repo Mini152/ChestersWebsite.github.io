c1163250bc62bb89a422bbb95656f6e4fcfd7308
Mini152 <60660223+Mini152@users.noreply.github.com>
2020-09-08 09:57:46 +0100

    Code.js now updates in realtime
    i previously did it the other way as proof of concept, now created it for propper use

--- Code.js
+++ Code.js
@@ -13,56 +13,65 @@ window.addEventListener("focus", resizeWindow);
 
 const txtArea = document.getElementById("txtArea");
 const bDisplayedCodeTitle = document.getElementById("bDisplayedCodeTitle");
+const generalURL = "https://raw.githubusercontent.com/Mini152/mini152.github.io/master/";
 
 // create initial identifier
 var name = "index";
-var type = "HTML";
-var identifier = name + type;
+var type = "html";
+var identifier = name + "." + type;
 
 function selectFileName(selectedName) {
     name = selectedName;
-    identifier = name + type;
+    identifier = name + "." + type;
     bDisplayedCodeTitle.innerText = identifier;
     getData();
 }
 
 function selectFileType(selectedType) {
     type = selectedType;
-    identifier = name + type;
+    identifier = name + "." + type;
     bDisplayedCodeTitle.innerText = identifier;
     getData();
 }
 
-function getData() {
-    // fetch json from Code.json
-    fetch('./Code.json')
-    .then(response => {
-        // handle responce (dont 100% know how this works)
-        console.log(response);
-        return response.json();
-    }).then(data => {
-        // json data can be handled here
-        console.log(data);
-        // loop through all of json finding correct identifier
-        for (let i = 0; i < data.code.length; i++) {
-            if (data.code[i].identifier == identifier) {
-                // format code
-                let fileContent = data.code[i].content;
-                function replaceAll(string, search, replace) {
-                    return string.split(search).join(replace);
-                }
-                // format properly
-                fileContent = replaceAll(fileContent, "<qu>", '"');
-                fileContent = replaceAll(fileContent, "<id>", "    ");
-                // paste code into text area
-                txtArea.value = fileContent;
-                return;
-            }
-        }
-        console.log(data.code[0].content)
-    }).catch(error => {
-        console.log(error);
-    });   
+async function getData() {
+    let url = generalURL + identifier;
+    const html = (await (await fetch(url)).text());
+    txtArea.value = html;
 }
 
-getData();
+getData();    
+
+
+
+// legacy code (needed for somewhere else)
+
+//// fetch json from Code.json
+//fetch('./Code.json')
+//.then(response => {
+//    // handle responce (dont 100% know how this works)
+//    console.log(response);
+//    return response.json();
+//}).then(data => {
+//    // json data can be handled here
+//    console.log(data);
+//    // loop through all of json finding correct identifier
+//    for (let i = 0; i < data.code.length; i++) {
+//        if (data.code[i].identifier == identifier) {
+//            // format code
+//            let fileContent = data.code[i].content;
+//            function replaceAll(string, search, replace) {
+//                return string.split(search).join(replace);
+//            }
+//            // format properly
+//            fileContent = replaceAll(fileContent, "<qu>", '"');
+//            fileContent = replaceAll(fileContent, "<id>", "    ");
+//            // paste code into text area
+//            txtArea.value = fileContent;
+//            return;
+//        }
+//    }
+//    console.log(data.code[0].content)
+//}).catch(error => {
+//    console.log(error);
+//}); 
