23d01ce5f5b7626e3dbbd74a4da3f75850fc1ba0
Mini152 <60660223+Mini152@users.noreply.github.com>
2020-09-30 10:20:33 +0100

    Calendar can now change months

--- Calendar.js
+++ Calendar.js
@@ -1,19 +1,22 @@
 const dateTime = new Date;
 var daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-var daysInTheMonth = daysInMonths[dateTime.getMonth()];
+var selectedMonth = dateTime.getMonth();
+var selectedYear = dateTime.getFullYear();
+var daysInTheMonth = daysInMonths[selectedMonth];
 
-//events
-
-// initialising calendar
-function init() {
+function renderCalendar() {
     var bMonthTitle = document.getElementById("bMonthTitle");    
-
-    var months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];     
-    var currentMonth = months[dateTime.getMonth()];
+    var months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
     // make title = current month
-    var currentMonth = months[dateTime.getMonth()];      
-    bMonthTitle.innerText = currentMonth;
+    bMonthTitle.innerText = months[selectedMonth] + "  " + selectedYear;
+
+    // clear up dayDiv(s)
+    for (let i = 0; i <= 36; i++) {
+        document.getElementById("divDayTitle" + i).innerText = "";
+        document.getElementById("divDay" + i).style.visibility = "visible";
+        document.getElementById("divDay" + i).style.borderColor = "#68C232";
+    }
 
     // days of the month displayed
     for (let i = 1; i <= daysInTheMonth; i++) {
@@ -25,16 +28,17 @@ function init() {
         if (document.getElementById("divDayTitle" + i).innerText == "") {
             document.getElementById("divDay" + i).style.visibility = "hidden";
         }
-
     }
 
-    // highlighting today
-    document.getElementById("divDay" + ((dateTime.getDate() - 1) + getTheFirstDayOfMonth())).style.borderColor = "red"; // -1 because date starts on 1 and calender starts on 0
+    if (selectedMonth == dateTime.getMonth()) {
+        // highlighting today
+        document.getElementById("divDay" + ((dateTime.getDate() - 1) + getTheFirstDayOfMonth())).style.borderColor = "red"; // -1 because date starts on 1 and calender starts on 0
+    }
 }
 
 function getTheFirstDayOfMonth() {
-    var month = dateTime.getMonth();
-    var year = dateTime.getFullYear();
+    var month = selectedMonth;
+    var year = selectedYear;
     var firstDay = new Date(year, month, 1);
     var firstDayOfMonth = firstDay.getDay() - 1; //-1 because sunday = 0 so all days shifted by 1 left
     if (firstDayOfMonth == -1) {
@@ -43,4 +47,26 @@ function getTheFirstDayOfMonth() {
     return firstDayOfMonth; 
 }
 
-init();
+function nextMonth() {
+    console.log("next");
+    if (selectedMonth == 11) {
+        selectedMonth = 0;
+        selectedYear++;
+    }
+    else selectedMonth++;
+    daysInTheMonth = daysInMonths[selectedMonth];
+    renderCalendar();
+}
+
+function lastMonth() {
+    console.log("prev");
+    if (selectedMonth == 0) {
+        selectedMonth = 11;
+        selectedYear--;
+    }
+    else selectedMonth--;
+    daysInTheMonth = daysInMonths[selectedMonth];
+    renderCalendar();
+}
+
+renderCalendar();
